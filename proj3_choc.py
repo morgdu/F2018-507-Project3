cce8e09580b8a0d4ac19231e985014d585563c14
morgdu <38295108+morgdu@users.noreply.github.com>
2018-11-27 15:04:21 -0500

    process_command implemented

--- proj3_choc.py
+++ proj3_choc.py
@@ -105,3 +105,233 @@ def populate_tables():
 
 create_tables()
 populate_tables()
+
+# Part 2: Implement logic to process user commands
+def process_command(command):
+
+    conn = sqlite3.connect(DBNAME)
+    cur = conn.cursor()
+
+    # separate primary command from parameters
+    cmnd = command.split()[0]
+    params = command.split()[1:]
+
+    # bars prompt
+    if cmnd == "bars":
+
+        # set defaults
+        country_region = ""
+        order_by = "Rating"
+        order = "DESC"
+        num = 10
+
+        # check for parameters in the command line and adjust sql query accordingly
+        for param in params:
+            if "sellcountry" in param:
+                country_region = param.split('=')[1]
+                where_sql = " WHERE c.alpha2 = " + "'" + country_region + "'"
+            elif "sourcecountry" in param:
+                country_region = param.split('=')[1]
+                where_sql = " WHERE c2.alpha2 = " +  "'" + country_region + "'"
+            elif "sellreigion" in param:
+                country_region = param.split('=')[1]
+                where_sql = " WHERE c.Region = " + "'" + country_region + "'"
+            elif "sourceregion" in param:
+                country_region = param.split('=')[1]
+                where_sql = " WHERE c2.Region = " + "'" + country_region + "'"
+            elif "ratings" in param:
+                order_by = "Rating"
+            elif "cocoa" in param:
+                order_by = "CocoaPercent"
+            elif "top" in param:
+                order = "DESC"
+                num = param.split('=')[1]
+            elif "bottom" in param:
+                order = "ASC"
+                num = param.split('=')[1]
+            else:
+                return "Command not recognized: " + command
+    
+        # base statement that selects values from table joins
+        statement = "SELECT b.SpecificBeanBarName, b.Company, c.EnglishName, b.Rating, b.CocoaPercent, c2.EnglishName FROM Bars b JOIN Countries c on b.CompanyLocationId = c.Id JOIN Countries c2 on b.BroadBeanOriginId = c2.Id"
+
+        # if country value is a parameter, add where clause to SQL statement
+        if country_region != "":
+            statement += where_sql
+
+        # add order by, direction, and limit to SQL statement (either default or specified by params)
+        statement += (" ORDER BY " + order_by + " " + order + " LIMIT " + str(num))
+
+        # get results
+        results = cur.execute(statement)
+        results = cur.fetchall()
+
+        return results
+
+    # companies prompt
+    elif cmnd == "companies":
+        
+        # set defaults
+        country_region = ""
+        order_by = "Rating"
+        order = "DESC"
+        num = 10
+        
+        # check for parameters in the command line and adjust sql query accordingly
+        for param in params:
+            if "country" in param:
+                country_region = param.split('=')[1]
+                where_sql = " WHERE c.alpha2 = " + "'" + country_region + "'"
+            elif "region" in param:
+                country_region = param.split('=')[1]
+                where_sql = " WHERE c.Region = " +  "'" + country_region + "'"
+            elif "ratings" in param:
+                order_by = "Rating"
+                aggregate = "AVG(b.Rating)"
+            elif "cocoa" in param:
+                order_by = "CocoaPercent"
+                aggregate = "AVG(b.CocoaPercent)"
+            elif "bars_sold" in param:
+                order_by = "COUNT(*)"
+                aggregate = "COUNT(*)"
+            elif "top" in param:
+                order = "DESC"
+                num = param.split('=')[1]
+            elif "bottom" in param:
+                order = "ASC"
+                num = param.split('=')[1]
+            else:
+                return "Command not recognized: " + command
+    
+        # base statement that selects correct values from table and joins
+        statement = "SELECT b.Company, c.EnglishName," + aggregate + " FROM Bars b JOIN Countries c on b.CompanyLocationId = c.Id"
+        # if country value is a parameter, add where clause to SQL statement
+    
+        if country_region != "":
+            statement += where_sql
+
+        # update with group by statement 
+        statement += " GROUP BY b.Company HAVING COUNT(*) > 4"
+
+        # add order by, direction, and limit to SQL statement (either default or specified by params)
+        statement += (" ORDER BY " + aggregate + " " + order + " LIMIT " + str(num))
+
+        # get results
+        results = cur.execute(statement)
+        results = cur.fetchall()
+
+        return results
+
+    # countries prompt
+    elif cmnd == "countries":
+        
+        # set defaults
+        country_region = ""
+        join_by = " FROM Bars b JOIN Countries c on b.CompanyLocationId = c.Id"
+        order_by = "Rating"
+        order = "DESC"
+        num = 10
+        
+        # check for parameters in the command line and adjust sql query accordingly
+        for param in params:
+            if "region" in param:
+                country_region = param.split('=')[1]
+                where_sql = " WHERE c.Region = " +  "'" + country_region + "'"
+            elif "sellers" in param:
+                join_by = join_by
+            elif "sources" in param:
+                join_by = " FROM Bars b JOIN Countries c on b.BroadBeanOriginId = c.Id"
+            elif "ratings" in param:
+                order_by = "Rating"
+                aggregate = "AVG(b.Rating)"
+            elif "cocoa" in param:
+                order_by = "CocoaPercent"
+                aggregate = "AVG(b.CocoaPercent)"
+            elif "bars_sold" in param:
+                order_by = "COUNT(*)"
+                aggregate = "COUNT(*)"
+            elif "top" in param:
+                order = "DESC"
+                num = param.split('=')[1]
+            elif "bottom" in param:
+                order = "ASC"
+                num = param.split('=')[1]
+            else:
+                return "Command not recognized: " + command
+    
+        # base statement that selects correct values from table and joins
+        statement = "SELECT c.EnglishName, c.Region," + aggregate + join_by
+        
+        # if country/region value is a parameter, add where clause to SQL statement
+        if country_region != "":
+            statement += where_sql
+
+        # update with group by statement 
+        statement += " GROUP BY c.EnglishName HAVING COUNT(*) > 4"
+
+        # add order by, direction, and limit to SQL statement (either default or specified by params)
+        statement += (" ORDER BY " + aggregate + " " + order + " LIMIT " + str(num))
+
+        # get results
+        results = cur.execute(statement)
+        results = cur.fetchall()
+
+        return results
+
+    # regions prompt
+    elif cmnd == "regions":
+        
+        # set defaults
+        country_region = ""
+        join_by = " FROM Bars b JOIN Countries c on b.CompanyLocationId = c.Id"
+        order_by = "Rating"
+        order = "DESC"
+        num = 10
+        
+        # check for parameters in the command line and adjust sql query accordingly
+        for param in params:
+            if "sellers" in param:
+                join_by = join_by
+            elif "sources" in param:
+                join_by = " FROM Bars b JOIN Countries c on b.BroadBeanOriginId = c.Id"
+            elif "ratings" in param:
+                order_by = "Rating"
+                aggregate = "AVG(b.Rating)"
+            elif "cocoa" in param:
+                order_by = "CocoaPercent"
+                aggregate = "AVG(b.CocoaPercent)"
+            elif "bars_sold" in param:
+                order_by = "COUNT(*)"
+                aggregate = "COUNT(*)"
+            elif "top" in param:
+                order = "DESC"
+                num = param.split('=')[1]
+            elif "bottom" in param:
+                order = "ASC"
+                num = param.split('=')[1]
+            else:
+                return "Command not recognized: " + command
+    
+        # base statement that selects correct values from table and joins
+        statement = "SELECT c.Region," + aggregate + join_by
+
+        # update sql with group by statement 
+        statement += " GROUP BY c.Region HAVING COUNT(*) > 4"
+
+        # add order by, direction, and limit to SQL statement (either default or specified by params)
+        statement += (" ORDER BY " + aggregate + " " + order + " LIMIT " + str(num))
+
+        # get results
+        results = cur.execute(statement)
+        results = cur.fetchall()
+
+        return results
+    
+    # if any other word present in command, return bad input error
+    else:
+        return "Command not recognized: " + command
+
+
+def load_help_text():
+    with open('help.txt') as f:
+        return f.read()
